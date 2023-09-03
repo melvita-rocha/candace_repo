6c5a39d324553fbef0468999a77931838747a4a4
Candace <dias.candace.a@gmail.com>
2023-09-03 11:34:37 +0530

    jobs working

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+	img= models.ImageField(upload_to='images/') #whenever uploaded where to save
+	summary = models.CharField(max_length=200) #only 200 words
